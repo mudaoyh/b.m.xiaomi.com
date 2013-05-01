9651ffcfc3f322018bfb72d0dded49fb1c7fea6a
macaienlei <macaienlei@gmail.com>
2013-05-01 20:38:09 +0800

    productView

--- app/scripts/views/product/productView.js
+++ app/scripts/views/product/productView.js
@@ -6,9 +6,34 @@ define([
     'underscore',
     'backbone',
     'libs/mipu',
-    'libs/util'
-], function($, _, Backbone, Mipu, Util){
+    'libs/util',
+    'text!templates/product/productViewTemplate.html'
+], function($, _, Backbone, Mipu, Util, ProductViewTemplate){
     var ProductView = Backbone.View.extend({
+        el: $('#viewbody'),
+        initialize: function(){
+            console.log('productView render');
+        },
+        render: function(product_id){
+            this.options.product_id = product_id;
+            this.process(function(self){
+                var compileTemplate = $.tmpl(ProductViewTemplate, self.options.res);
+                self.$el.html(compileTemplate);
+            });
+        },
+        process: function(callback){
+            var options = {
+                url: '/product/view',
+                param: {
+                    'product_id': this.options.product_id
+                },
+                that: this
+            };
+            Mipu.request(options, function(res, self){
+                self.options.res = res.data.result;
+                callback(self);
+            });
+        }
     });
-    return ProductView;
+    return new ProductView;
 });
