f258d198e15fff865e401b80eb8796d9c464cfc0
macaienlei <macaienlei@gmail.com>
2013-05-02 18:03:13 +0800

    add jquery.lazyload

--- app/scripts/views/product/productView.js
+++ app/scripts/views/product/productView.js
@@ -30,7 +30,22 @@ define([
                 that: this
             };
             Mipu.request(options, function(res, self){
-                self.options.res = res.data.result;
+                var product, selectConsumptionArray, i, k, stylelist;
+
+                product = res.data.result;
+                selectConsumptionArray = [];
+
+                for(i = 1,k=parseInt(product.buy_limit); i<=k; i+=1){
+                    selectConsumptionArray.push(i);
+                }
+                // 购买限制 buy_limit
+                product.selectConsumptionArray = selectConsumptionArray;
+
+                product.stylelength = 0;
+                for(stylelist in product.style){
+                    product.stylelength += 1;
+                }
+                self.options.res = product;
                 callback(self);
             });
         }
